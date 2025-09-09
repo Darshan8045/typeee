a819566bfe9ae1b34bf2a2ecd8a56a6ac501ac3b
Darshan8045 <230701062@rajalakshmi.edu.in>
2025-09-09 20:52:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",(event)=>{
+
+ document.getElementById("highlightBtn").addEventListener("click",()=>{
+
+ const btnclass=document.getElementById("textPara");
+
+ btnclass.classList.toggle("highlight");
+
+ 
+
+ })
+
+})
